6a9b89e48ebe364d715e6938165de77e87606931
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-01-13 16:58:45 +0530

    Changed for testing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Shop from './Components/Shop';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
         <Routes>
           <Route element={<Main />} path="/" />
           <Route element={<Shop />} path="/Shop" />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
