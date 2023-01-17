8da4508002e6d1205a840f4acfebac75793f7397
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-01-17 19:07:03 +0530

    Created header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Shop from './Components/Shop';
+import Footer from './Components/Footer';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <Routes>
-          <Route element={<Main />} path="/" />
-          <Route element={<Shop />} path="/Shop" />
-        </Routes>
+      <Routes>
+        <Route element={<Main />} path="/" />
+        <Route element={<Shop />} path="/Shop" />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,23 @@
-const Header = () =>{
-    return (
-        <div className="Header">
-            <h1>HEADER</h1>
+import { Link } from 'react-router-dom';
+import './Header.css';
+import valorant_logo from '../images/valorant-logo.png';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="title">
+        <div className="logo">
+          <img src={valorant_logo} alt="Logo" className="destiny_logo" />
         </div>
-    )
-}
+        <Link to={'/'}>VALORANT STORE</Link>
+      </div>
+      <div className="links">
+        <Link to={'/'}>HOME</Link>
+        <Link to={'/shop'}>SHOP</Link>
+        <Link to={'/about'}>ABOUT</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
