9f0d02122fa0b7de8ceb44602846064e38429403
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-01-17 19:07:31 +0530

    created main page ui

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,34 @@
-const Main = () =>{
-    return (
-        <div className="Main">
-            <h1>MAIN</h1>
+import './Main.css';
+import valorant_logo from '../images/valorant-logo.png';
+import { Link } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <div className="left-div">
+        <span className="main-heading">
+          Welcome to <br /> Valorant store <br />
+        </span>
+        <span className="sub-text">
+          An extensive Shop containing <br /> most in-game items, assets and more.
+        </span>
+        <div className="shop-button-container">
+          <Link to={'/shop'}>
+            <button type="submit" className="shop-button">
+              SHOP
+            </button>
+          </Link>
         </div>
-    )
-}
+      </div>
+
+      <div className="right-div">
+        <div className="main-logo">
+          <img src={valorant_logo} alt="Valorant" className="valorant-logo" />
+        </div>
+        <div className="main-text">STORE</div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
