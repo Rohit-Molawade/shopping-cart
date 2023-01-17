086f4bd0a61e7b6f58263fb05c8d19826d5aa76c
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-01-17 19:07:46 +0530

    created footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <span>
+        API and UI provided by <a href="http://www.valorant-api.com"> valorant-api.com</a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
