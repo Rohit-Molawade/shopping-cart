86e806186077cb3557db9d4d4af8d4c3feaebbab
Rohit-Molawade <rohit.molawade27@gmail.com>
2023-01-22 03:11:56 +0530

    Added Component to show empty cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -20,39 +20,50 @@ const Cart = (props) => {
   };
 
   return (
-    <div className="Cart">
-      <div className="top">
-        <p>YOUR SHOPPING CART</p>
-      </div>
-      <div className="middle">
-        <div className="heading">
-          <h3>ITEM</h3>
-          <h3>INFO</h3>
-          <h3>COST</h3>
-          <h3>AMOUNT</h3>
-          <h3>SUBTOTAL</h3>
+    <>
+      {itemArray.length !== 0 ? (
+        <div className="Cart">
+          <div className="top">
+            <p>YOUR SHOPPING CART</p>
+          </div>
+          <div className="middle">
+            <div className="heading">
+              <h3>ITEM</h3>
+              <h3>INFO</h3>
+              <h3>COST</h3>
+              <h3>AMOUNT</h3>
+              <h3>SUBTOTAL</h3>
+            </div>
+            <div className="item-container">
+              {itemArray.map((item) => {
+                return <Item key={item.Name} item={item} setItemArray={setItemArray} setTotalAmount={setTotalAmount} itemArray={itemArray} />;
+              })}
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="total-amount-container">
+              <p className="total-text">TOTAL</p>
+              <p className="total-amount">{totalAmount}</p>
+            </div>
+            <div className="buttons-container">
+              <Link to={'/'}>
+                <button type="button">CHECKOUT</button>
+              </Link>
+              <Link to={'/shop'}>
+                <button type="button">SHOP MORE</button>
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="item-container">
-          {itemArray.map((item) => {
-            return <Item key={item.Name} item={item} setItemArray={setItemArray} setTotalAmount={setTotalAmount} itemArray={itemArray} />;
-          })}
-        </div>
-      </div>
-      <div className="bottom">
-        <div className="total-amount-container">
-          <p className="total-text">TOTAL</p>
-          <p className="total-amount">{totalAmount}</p>
-        </div>
-        <div className="buttons-container">
-          <Link to={'/'}>
-            <button type="button">CHECKOUT</button>
-          </Link>
+      ) : (
+        <div className="empty-cart">
+          <h1>Your shopping cart is empty</h1>
           <Link to={'/shop'}>
-            <button type="button">SHOP MORE</button>
+            <button type="button">SHOP NOW</button>
           </Link>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
